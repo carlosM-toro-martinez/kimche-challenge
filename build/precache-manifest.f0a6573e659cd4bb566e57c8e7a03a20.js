4e27da82527b0396d739f58b50a61d4f03cc3892
Carlos Toro <carlostorom.96@gmail.com>
2022-05-18 16:04:46 -0400

    build: deploy produccion

---
+++ build/precache-manifest.f0a6573e659cd4bb566e57c8e7a03a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5708e680e1d6a1563d62eb1ddc9969e7",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/index.html"
+  },
+  {
+    "revision": "b7bd67d712226d91ca8c",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/css/2.4253ffa8.chunk.css"
+  },
+  {
+    "revision": "9ce3ca3f75eb11a59607",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/css/main.7a9966f9.chunk.css"
+  },
+  {
+    "revision": "b7bd67d712226d91ca8c",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/js/2.4060f257.chunk.js"
+  },
+  {
+    "revision": "974be8b99d187d737190e211a6935021",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/js/2.4060f257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce3ca3f75eb11a59607",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/js/main.b85d8049.chunk.js"
+  },
+  {
+    "revision": "ba958c0a4221dab4cb18",
+    "url": "/https//:carlosM-toro-martinez.githu.io/kimche-challenge/static/js/runtime-main.1958c526.js"
+  }
+]);
